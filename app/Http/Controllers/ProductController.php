a310d066c57b5beb756196654de9e18f41e2c542
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2024-03-15 23:55:31 +0200

    Feat: Enhance product management functionality by deleting the product images

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,23 +23,22 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(10);
+        $products = Product::all();
 
         if ($products->count() > 0) {
             foreach ($products as $product) {
                 // Retrieve and encode thumbnail
                 if ($product->thumbnail && file_exists(public_path($product->thumbnail))) {
-                    $thumbnailPath = public_path($product->thumbnail);
-                    $thumbnailData = file_get_contents($thumbnailPath);
-                    $product->thumbnail = base64_encode($thumbnailData);
+                    $thumbnailPath = $product->thumbnail;
+                    $product->thumbnail = $thumbnailPath;
                 }
 
                 // Retrieve and encode images
                 $productImages = $product->images()->pluck('image');
                 foreach ($productImages as $key => $imagePath) {
                     if (file_exists(public_path($imagePath))) {
-                        $imageData = file_get_contents(public_path($imagePath));
-                        $productImages[$key] = base64_encode($imageData);
+                        $imageData = $imagePath;
+                        $productImages[$key] = $imageData;
                     } else {
                         unset($productImages[$key]); // Remove non-existent image path
                     }
@@ -99,6 +98,8 @@ class ProductController extends Controller
         // $product->thumbnail = $request['thumbnail'];
         $product->category_id = $request['category_id'];
         $product->seller_id = $request['seller_id'];
+        $product->category()->associate($request['category_id']);
+        $product->seller()->associate($request['seller_id']);
 
         // if($request->hasFile('thumbnail')) {
         //     $path = 'assets/uploads/product/'. $product->thumbnail;
@@ -289,12 +290,40 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse JSON response indicating the success or failure of the deletion operation.
      */
+    // public function destroy(string $id)
+    // {
+    //     $product = Product::find($id);
+    //     if ($product) {
+    //         $product->images()->delete();
+    //         $product->delete();
+    //         return response()->json('Product deleted successfully', 200);
+    //     } else {
+    //         return response()->json('Product not found', 404);
+    //     }
+    // }
+
     public function destroy(string $id)
     {
         $product = Product::find($id);
         if ($product) {
+
+            // Delete the thumbnail
+            if (File::exists(public_path($product->thumbnail))) {
+                File::delete(public_path($product->thumbnail));
+            }
+
+            // Delete images from storage
+            foreach ($product->images as $productImage) {
+                $imagePath = public_path($productImage->image);
+                if (File::exists($imagePath)) {
+                    File::delete($imagePath);
+                }
+            }
+
+            // Delete product record
             $product->images()->delete();
             $product->delete();
+
             return response()->json('Product deleted successfully', 200);
         } else {
             return response()->json('Product not found', 404);
