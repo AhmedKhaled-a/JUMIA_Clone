c5eb54b9685f92e86b83ec05a1ea99443c8e3e24
Ahmad-Mohamad-Ibrahim <ahmadmohamadibrahim11@gmail.com>
2024-03-15 02:57:12 +0200

    adding content col to messages

--- database/migrations/2024_03_13_195459_create_messages_table.php
+++ database/migrations/2024_03_13_195459_create_messages_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('writer');
             $table->string('title');
+            $table->text('content');
             $table->unsignedBigInteger('product_id')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('product_id')->references('id')->on('products');
