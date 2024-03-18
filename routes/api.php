bea86b96b98104c6221232d7fcbd1e753d10ecb2
Ahmad-Mohamad-Ibrahim <ahmadmohamadibrahim11@gmail.com>
2024-03-19 01:31:16 +0200

    fix api.php

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
-<<<<<<< HEAD
 use App\Http\Controllers\SellerController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ViewedProductsController;
@@ -24,9 +23,6 @@ use App\Http\Controllers\ViewedProductsController;
 
 
 
-=======
-use App\Http\Controllers\UserController;
->>>>>>> a6fb0b197325a193e5aa43410665e9f7e4e6c065
 
 /*
 |--------------------------------------------------------------------------
