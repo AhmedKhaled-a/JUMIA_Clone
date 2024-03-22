ec627eea464590f5906402dbc91d930915b9bcb0
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2024-03-23 01:25:30 +0200

    minor changes in reset password functions

--- resources/views/sellerResetLink.blade.php
+++ resources/views/sellerResetLink.blade.php
@@ -6,7 +6,7 @@
 <body>
 
 <div>
-    
+
     <br>
     Welcome in jumia Clone!
     <br>
@@ -15,7 +15,8 @@
     Please click on the link below or copy it into the address bar of your browser to reset your password:
     <br>
 
-    <a href="{{ url('api/seller/resetPassword', $remember_token)}}">reset your password </a>
+    {{-- <a href="{{ url('api/seller/resetPassword', $remember_token)}}">reset your password </a> --}}
+    <a href="http://localhost:3000/seller/reset-Password/request/{{$remember_token}}">reset your password </a>
 
     <br/>
 </div>
