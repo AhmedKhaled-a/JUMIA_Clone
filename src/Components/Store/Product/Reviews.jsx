3128a33cdcea9b9c696b472c47cf4da8eb17ce43
Ahmad-Mohamad-Ibrahim <ahmadmohamadibrahim11@gmail.com>
2024-03-25 01:28:14 +0200

    fixing reviews

--- src/Components/Store/Product/Reviews.jsx
+++ src/Components/Store/Product/Reviews.jsx
@@ -29,32 +29,16 @@ function Reviews({ productId }) {
 
   return (
     <div>
-      {reviews?.length > 0 ? <h2>Product Reviews</h2> : ""}
-      {reviews?.map((review) => (
-
-        // <div key={review.id}>
-        //   <h3>{review.title}</h3>
-        //   <p> By:{review.writer}</p>
-        //   <p><Rating readOnly defaultValue={Math.round(review.rating * 2) / 2} precision={0.5}></Rating></p>
-        //   <p>{review.content}</p>
-        // </div>
+      {
+      reviews?.map((review) => (
         <div className="d-flex justify-content-start align-items-start bg-light p-4 rounded mb-5">
-          <div style={{ width: "170px", height: "170px" }} className="rounded bg-light shadow col-4 border me-4 d-flex flex-column justify-content-start align-items-center p-3">
-
-            {/* <p style={{ color: "#f68b2c", fontSize: "30px" }} className="text-center">{averageRating.toFixed(1)}/5</p>
-          <p className="py-0 my-0"><Rating readOnly defaultValue={averageRating.toFixed(1)} precision={0.5}></Rating></p> */}
-
-            <p className="mt-2">{reviews?.length} Reviews</p>
-          </div>
           <div className="bg-light p-3 rounded mb-4 col-8">
-            {reviews?.map((review) => (
-              <div key={review.id} className="border-bottom mt-2">
-                <p><Rating readOnly defaultValue={Math.round(review.rating * 2) / 2} precision={0.5}></Rating></p>
-                <h3>{review.title}</h3>
-                <p>{review.content}</p>
-                <p className="text-muted" style={{ fontSize: "14px" }}>{formatDate(review.created_at)} by {review.writer}</p>
-              </div>
-            ))}
+            <div key={review.id} className="border-bottom mt-2">
+              <p><Rating readOnly defaultValue={Math.round(review.rating * 2) / 2} precision={0.5}></Rating></p>
+              <h3>{review.title}</h3>
+              <p>{review.content}</p>
+              <p className="text-muted" style={{ fontSize: "14px" }}>{formatDate(review.created_at)} by {review.writer}</p>
+            </div>
           </div>
         </div>
       ))
@@ -62,5 +46,4 @@ function Reviews({ productId }) {
     </div>
   );
 }
-
 export default Reviews
